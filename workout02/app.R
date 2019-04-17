f4209f8c1d97fce38ed06ee4948b5226a6122439
Zishen Liu <zishenliu@berkeley.edu>
2019-04-17 15:18:00 -0700

    workout2:code change

--- workout02/app.R
+++ workout02/app.R
@@ -1,10 +1,10 @@
-# Lab 11, Stat 133 Spring 2018, Prof. Sanchez
-# Shiny App version 1: Drawing balls from boxes
-# Inputs:
-#   repetitions: number of repetitions
-#
-# Outputs:
-#   frequency plot of number of blue balls
+############################################
+# Savings and Annuity Simulator 
+# Zishen Liu
+# UC Berkeley 
+# Professor 
+# Statistics 133
+############################################
 
 library(shiny)
 library(ggplot2)
@@ -46,150 +46,149 @@ ui <- fluidPage(
              value = 20
            ))
   ),
-  fluidRow(column(
-    4,
-    sliderInput(
-      "annualcontrib",
-      label = "Annual Contribution",
-      min = 0,
-      max = 50000,
-      value = 2000
-    )
-    
-  ),
-  column(
-    4,
-    sliderInput(
-      "grate",
-      label = "Growth Rate (in %)",
-      min = 0,
-      max = 20,
-      value = 2
-    )
-  ),
-  column(4,
-  selectInput("facet",
-   "Facet?",
-   c("No",
-     "Yes"))))
-  ,
-
-    h4("Timelines"),
-    fluidRow(plotOutput("freqs_plot")),
-    h4("Balances"),
-    verbatimTextOutput("summary")
-    
-  
+  fluidRow(
+    column(
+      4,
+      sliderInput(
+        "annualcontrib",
+        label = "Annual Contribution",
+        min = 0,
+        max = 50000,
+        value = 2000
+      )
+      
+    ),
+    column(
+      4,
+      sliderInput(
+        "grate",
+        label = "Growth Rate (in %)",
+        min = 0,
+        max = 20,
+        value = 2
+      )
+    ),
+    column(4,
+           selectInput("facet",
+                       "Facet?",
+                       c("No",
+                         "Yes")))
   )
+  ,
+  
+  h4("Timelines"),
+  fluidRow(plotOutput("freqs_plot")),
+  h4("Balances"),
+  verbatimTextOutput("summary")
+  
+  
+)
 
-  
-  future_value <- function(amount, rate, years) {
-    fv = amount * (1 + rate) ^ years
-    return(fv)
-  }
-  
-  annuity <- function(contrib, rate, years) {
-    fva = contrib * ((1 + rate) ^ years - 1) / rate
-    return(fva)
-  }
-  
-  growing_annuity <- function(contrib, rate, growth, years) {
-    fvga = contrib * ((1 + rate) ^ years - (1 + growth) ^ years) / (rate - growth)
-    return(fvga)
-  }
-  
-  # Define server logic required to draw the plot
-  server <- function(input, output) {
+#' @title Future Value Function
+#' @description computes the future value of an investment
+#' @param amount initial invested amount
+#' @param rate annual rate of return
+#' @param years number of years
+#' @return the Future Value of an investment
 
-    output$freqs_plot <- renderPlot({
+future_value <- function(amount, rate, years) {
+  fv = amount * (1 + rate) ^ years
+  return(fv)
+}
 
-      n <- input$years + 1
-      no_contrib <- vector("double", n)
-      fixed_contrib <- vector("double", n)
-      growing_contrib <- vector("double", n)
-      
-      for (k in 1:n) {
-        no_contrib[k] <-
-          future_value(input$initialamount, input$rrate / 100, years = k - 1)
-        fixed_contrib[k] <-
-          future_value(input$initialamount, input$rrate / 100, years = k - 1) + annuity(input$annualcontrib, input$rrate /
-                                                                                          100, years = k -
-                                                                                          1)
-        growing_contrib[k] <-
-          future_value(input$initialamount, input$rrate / 100, years = k - 1) + growing_annuity(input$annualcontrib,
-                                                                                                input$rrate / 100,
-                                                                                                input$grate / 100,
-                                                                                                years = k -
-                                                                                                  1)
-      }
-      
-      modalities <-
-        data.frame(
-          year = c(0:input$years),
-          no_contrib = no_contrib,
-          fixed_contrib = fixed_contrib,
-          growing_contrib = growing_contrib
-        )
-      df <- modalities %>%
-        gather(Modality,
-               values,
-               no_contrib,
-               fixed_contrib,
-               growing_contrib)
-      
-      df <-
-        modalities %>% gather(modality,
+#' @title Future Value of Annuity Function
+#' @description computes the Future Value of Annuity
+#' @param contrib contributed amount
+#' @param rate annual rate of return
+#' @param years number of years
+#' @return the Future Value of Annuity
+
+annuity <- function(contrib, rate, years) {
+  fva = contrib * ((1 + rate) ^ years - 1) / rate
+  return(fva)
+}
+
+#' @title Future Value of Annuity Function
+#' @description computes the Future Value of Annuity
+#' @param contrib contributed amount
+#' @param rate annual rate of return
+#' @param years number of years
+#' @return the Future Value of Annuity
+
+growing_annuity <- function(contrib, rate, growth, years) {
+  fvga = contrib * ((1 + rate) ^ years - (1 + growth) ^ years) / (rate - growth)
+  return(fvga)
+}
+
+# Define server logic required to draw the plot
+server <- function(input, output) {
+  modalities <- reactive({
+    n <- input$years + 1
+    no_contrib <- vector("double", n)
+    fixed_contrib <- vector("double", n)
+    growing_contrib <- vector("double", n)
+    
+    for (k in 1:n) {
+      no_contrib[k] <-
+        future_value(input$initialamount, input$rrate / 100, years = k - 1)
+      fixed_contrib[k] <-
+        future_value(input$initialamount, input$rrate / 100, years = k - 1) + annuity(input$annualcontrib, input$rrate /
+                                                                                        100, years = k -
+                                                                                        1)
+      growing_contrib[k] <-
+        future_value(input$initialamount, input$rrate / 100, years = k - 1) + growing_annuity(input$annualcontrib,
+                                                                                              input$rrate / 100,
+                                                                                              input$grate / 100,
+                                                                                              years = k -
+                                                                                                1)
+    }
+    
+    modalities <-
+      data.frame(
+        year = c(0:input$years),
+        no_contrib = no_contrib,
+        fixed_contrib = fixed_contrib,
+        growing_contrib = growing_contrib
+      )
+  })
+  
+  output$freqs_plot <- renderPlot({
+    df <-
+      modalities() %>% gather(modality,
                               value,
                               no_contrib,
                               fixed_contrib,
                               growing_contrib)
-      
-      df$modality <-
-        factor(df$modality,
-               levels = c("no_contrib", "fixed_contrib", "growing_contrib"))
-      if (input$facet == 'Yes'){
-      ggplot(df, aes(x = year, y = value, colour = modality)) +
-        geom_line() + geom_point() + ggtitle("Three modes of Investing") + facet_wrap(~modality)+geom_area(aes(fill = modality, group = modality),
-                                                                                                                                                    alpha = 0.5)+theme_bw()
-      }else{
-        ggplot(df, aes(x = year, y = value, colour = modality)) +
-          geom_line() + geom_point() + ggtitle("Three modes of Investing") 
-      }
-        
-        })
     
-     output$summary <- renderPrint({
-       n <- input$years + 1
-       no_contrib <- vector("double", n)
-       fixed_contrib <- vector("double", n)
-       growing_contrib <- vector("double", n)
-       
-       for (k in 1:n) {
-         no_contrib[k] <-
-           future_value(input$initialamount, input$rrate / 100, years = k - 1)
-         fixed_contrib[k] <-
-           future_value(input$initialamount, input$rrate / 100, years = k - 1) + annuity(input$annualcontrib, input$rrate /
-                                                                                           100, years = k -
-                                                                                           1)
-         growing_contrib[k] <-
-           future_value(input$initialamount, input$rrate / 100, years = k - 1) + growing_annuity(input$annualcontrib,
-                                                                                                 input$rrate / 100,
-                                                                                                 input$grate / 100,
-                                                                                                 years = k -
-                                                                                                   1)
-       }
-       
-       modalities <-
-         data.frame(
-           year = c(0:input$years),
-           no_contrib = no_contrib,
-           fixed_contrib = fixed_contrib,
-           growing_contrib = growing_contrib
-         )
-       head(modalities,input$years+1)
-     })
-  }
+    df$modality <-
+      factor(df$modality,
+             levels = c("no_contrib", "fixed_contrib", "growing_contrib"))
+    if (input$facet == 'Yes') {
+      ggplot(df, aes(
+        x = year,
+        y = value,
+        colour = modality
+      )) +
+        geom_line() + geom_point() + ggtitle("Three modes of Investing") + facet_wrap( ~
+                                                                                         modality) + geom_area(aes(fill = modality, group = modality),
+                                                                                                               alpha = 0.5) +
+        theme_bw()
+    } else{
+      ggplot(df, aes(
+        x = year,
+        y = value,
+        colour = modality
+      )) +
+        geom_line() + geom_point() + ggtitle("Three modes of Investing")
+    }
+    
+  })
   
-  
-  # Run the application
-  shinyApp(ui = ui, server = server)
+  output$summary <- renderPrint({
+    head(modalities(), input$years + 1)
+  })
+}
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
